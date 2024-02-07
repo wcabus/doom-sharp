582a41ed6d215a519012724df501a3353d963c0c
Wesley Cabus <wesley@gotsharp.be>
2024-02-08 00:00:38 +0100

    Added unsafe vs safe benchmark

--- benchmarks/DoomSharp.Benchmarks.Runner/Program.cs
+++ benchmarks/DoomSharp.Benchmarks.Runner/Program.cs
@@ -1,5 +1,8 @@
 ﻿using BenchmarkDotNet.Running;
 using DoomSharp.Benchmarks.Runner.Workloads;
 
+var test = new UnsafeVsSafe();
+
 // BenchmarkRunner.Run<ArrayClearVsNewInstance>();
-BenchmarkRunner.Run<StructsVsClasses>();
+// BenchmarkRunner.Run<StructsVsClasses>();
+BenchmarkRunner.Run<UnsafeVsSafe>();
